5a851b511831ecd9eb7402a97fe892a7326d341f
Ainur <a.i.um@mail.ru>
2020-04-06 16:54:02 +0300

    000000000000000000

---
+++ КУРСАЧИ/Test[2]/Test[2]/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Test_2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 500);
+            this.MaximumSize = new System.Drawing.Size(460, 500);
+            this.MinimumSize = new System.Drawing.Size(460, 500);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label help;
+        private System.Windows.Forms.Label help1;
+        private System.Windows.Forms.Label help2;
+        private System.Windows.Forms.Label help3;
+        private System.Windows.Forms.Label help4;
+        private System.Windows.Forms.Label help5;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label back;
+        private System.Windows.Forms.Button regist;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.MaskedTextBox maskedtBox1;
+        private System.Windows.Forms.TextBox tx1;
+        private System.Windows.Forms.TextBox tx2;
+        private System.Windows.Forms.TextBox surname;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox phone;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.CheckBox ch1;
+        private System.Windows.Forms.ComboBox combox;
+    }
+}
+
